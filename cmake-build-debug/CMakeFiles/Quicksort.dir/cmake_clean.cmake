8d6d81a5d8d2f3e416e54091d7957c65b578dcf4
NikolfaxW <denisnik@cvut.cz>
2026-05-25 19:39:05 +0200

    Original3

---
+++ cmake-build-debug/CMakeFiles/Quicksort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Quicksort.dir/main.cpp.o"
+  "CMakeFiles/Quicksort.dir/main.cpp.o.d"
+  "Quicksort"
+  "Quicksort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Quicksort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
